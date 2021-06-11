f7692ebf73869ce61b4394dd515080e5faaba96d
Hagar Usama <eng-hagar.usama1621@alexu.edu.eg>
2021-06-11 14:19:53 +0200

    Revert "Ros integration"

--- src/camFusion_Student.cpp
+++ src/camFusion_Student.cpp
@@ -298,7 +298,9 @@ void computeTTCCamera(std::vector<cv::KeyPoint> &kptsPrev, std::vector<cv::KeyPo
     medianDistRatio = getMedianCam(distRatios);
     double dT = 1 / frameRate;
     TTC = -dT / (1 - medianDistRatio);
-    // cout << "Camera TTC: " << TTC << " s" << endl;
+    cout << "Camera TTC: " << TTC << " s" << endl;
+    writeLog("results.csv", to_string(TTC));
+    writeLog("results.csv", "\n");
 }
 
 void computeTTCLidar(std::vector<LidarPoint> &lidarPointsPrev,
@@ -315,7 +317,9 @@ void computeTTCLidar(std::vector<LidarPoint> &lidarPointsPrev,
 
     // compute TTC from both measurements
     TTC = minXCurr * dT / (minXPrev - minXCurr);
-    // cout << "Lidar TTC: " << TTC << " s" << endl;
+    cout << "Lidar TTC: " << TTC << " s" << endl;
+    writeLog("results.csv", to_string(TTC));
+    writeLog("results.csv", ",");
 }
 
 int getPrevBoxID(DataFrame prevFrame, cv::DMatch match)
